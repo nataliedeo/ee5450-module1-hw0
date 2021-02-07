c6cee3808e19719fddc604668870cddbd4abf6fd
Curtis Wang <ycwang@u.northwestern.edu>
2021-02-06 23:43:29 -0800

    forgot a comma in print_dealer_full()

--- blackjack.py
+++ blackjack.py
@@ -200,7 +200,7 @@ class Blackjack(object):
         print(f"Dealer: {self._dealer_stack[0]}")
 
     def print_dealer_full(self):
-        print(f"Dealer: {''.join([str(card) for card in self._dealer_stack])}"
+        print(f"Dealer: {''.join([str(card) for card in self._dealer_stack])}",
               f" at sum {self._calculate_stack_sum(self._dealer_stack)}")
 
     def print_player_stack(self, player_idx: int):
